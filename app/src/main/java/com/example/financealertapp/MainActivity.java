fb44de5b22f95a0ee1d1a559b3d0cfa3ba517fb0
Abhishek Patel <38337132+PatelProjects@users.noreply.github.com>
2020-04-14 00:37:14 -0400

    Added navagation skeleton and fixed notification

--- app/src/main/java/com/example/financealertapp/MainActivity.java
+++ app/src/main/java/com/example/financealertapp/MainActivity.java
@@ -14,10 +14,13 @@ import android.widget.Button;
 import android.view.View;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btNotification;
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = "HHHHIIIII";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     public void addNotification(){
